8f3cc45d882d0302747c33c763977edc81f374fb
Eugenio Ruiz <eugenio_rm@outlook.es>
2023-05-04 10:41:16 +0200

    Añadida explicación

--- src/Principal.java
+++ src/Principal.java
@@ -5,7 +5,8 @@ public class Principal {
 		// TODO Auto-generated method stub
 
 		//Esta es la clase principal de mi proyecto de lo que sea
-		//para la asignatura de ticII   
+		//para la asignatura de ticII 
+		//Aquí se incluye el funcionamiento
 		
 		
 		
